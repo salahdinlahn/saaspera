99e42aebd774d2cda0c615f77fe51e1009afabfe
SalahdinLahnoud2 <lahnoudsalahdin1978@gmail.com>
2025-02-03 20:49:34 +0100

    Initial commit - Spring Boot Hello World

---
+++ src/main/java/com/saaspera/project/SaasperaApplication.java
@@ -0,0 +1,13 @@
+package com.saaspera.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaasperaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaasperaApplication.class, args);
+	}
+
+}
